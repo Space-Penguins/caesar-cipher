94df3bfbe45650c78689bc6da6cd8f96f49f3da0
Space-Penguins <hampus.berg@pm.me>
2025-10-17 14:58:22 +0200

    takes two args, string and shift

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
+import argparse
 
-# Take user string and shift
-# Shift every letter in string 
-# Return string
+def main():
+    # Take user string and shift 
+    parser = argparse.ArgumentParser(
+        description="Ceasar cipher encryption/decryption")
+    parser.add_argument(
+        "input", help="string to be encrypted/decrypted, A-Z", type=str)
+    parser.add_argument(
+        "shift", help="integer, positive or negative", type=int)
+    
+    args = parser.parse_args()
+    print(f"input: {args.input}")               
+    print(f"shift: {args.shift}")               
+    # Shift every letter in string 
+    # Return string
+
+if __name__ == "__main__":
+    main()
