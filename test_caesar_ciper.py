49a20dc4607a5e7d36b59b266eb8dfd9feaadcec
Space-Penguins <hampus.berg@pm.me>
2025-10-17 16:28:25 +0200

    Added test where the shift should loop back to the beginning of the alphabet, currently not working

--- test_caesar_ciper.py
+++ test_caesar_ciper.py
@@ -6,6 +6,9 @@ class TestCaesarCipher(unittest.TestCase):
 
     def test_cipher(self):
         self.assertEqual(caesarCipher("abc",1),"bcd")
+        self.assertEqual(caesarCipher("ABC",1),"BCD")
+        self.assertEqual(caesarCipher("xyz",1),"yza")
+
         
 if __name__ == '__main__':
     unittest.main()
