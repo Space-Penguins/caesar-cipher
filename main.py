8d5032034e7890c6ce03414058f8cbd55cf9ca06
Space-Penguins <hampus.berg@pm.me>
2025-10-17 15:26:44 +0200

    simple shift

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import argparse
 
 def main():
-    # Take user string and shift 
     parser = argparse.ArgumentParser(
         description="Ceasar cipher encryption/decryption")
     parser.add_argument(
@@ -12,9 +11,17 @@ def main():
     
     args = parser.parse_args()
     print(f"input: {args.input}")               
-    print(f"shift: {args.shift}")               
-    # Shift every letter in string 
-    # Return string
+
+    caesarCipher = "" 
+    for letter in args.input:
+        if not letter.isalpha():
+            caesarCipher += characterShift
+            continue
+        ordinalShift = ord(letter) + args.shift
+        characterShift = chr(ordinalShift)
+        caesarCipher += characterShift
+
+    print(caesarCipher)
 
 if __name__ == "__main__":
     main()
